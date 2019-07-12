d6254992297134a5ec566255ac36b20068a162f5
sverona <sasham@fastmail.com>
2019-07-12 04:44:44 -0500

    update documentation and set boxes to proper resolution

--- corpus/sync_match.py
+++ corpus/sync_match.py
@@ -22,10 +22,39 @@ VALID_FRAMES = [99, 98, 96, 94, 93, 91,
                 19, 17, 16, 14, 12, 11,
                 9, 7, 6, 4, 2, 0]
 
+
 def is_valid(time):
-    return len(time) == 6 and (int(time[4:]) in VALID_FRAMES)
+    """Return whether the given string represents a frame that can occur
+    on Melee's timer.
+
+    Parameters
+    ----------
+    time : str
+        A string.
+    """
+    return (len(time) == 6 and time.isnumeric()
+            and int(time[4:]) in VALID_FRAMES)
+
 
 def distance(fr, to):
+    """Return the number of frames after the valid timestamp `fr` at which
+    the valid timestamp `to` will appear.
+
+    Parameters
+    ----------
+    fr : str
+        A six-digit string representing a valid timestamp. (This would have
+        been named "from" if it weren't a Python reserved word.)
+    to : str
+        A six-digit string representing a valid timestamp.
+
+    Returns
+    -------
+    int
+        The directed distance, in frames, from `fr` to `to`. Positive if `fr`
+        is the higher of the two.
+    """
+
     if not (is_valid(fr) and is_valid(to)):
         return None
 
@@ -33,25 +62,60 @@ def distance(fr, to):
     seconds = int(to[2:4]) - int(fr[2:4])
     frames = VALID_FRAMES.index(int(to[4:])) - VALID_FRAMES.index(int(fr[4:]))
 
-    return minutes * 60 * 60 + seconds * 60 - frames
+    # We subtract `frames` because the array of valid frames is in reverse.
+    return (minutes * 60 * 60) + (seconds * 60) - frames
 
-def get_digit_image(n, small=False, color=False, white=False):
-    """Return the asset corresponding to the appropriate timer digit as an
-    OpenCV-compatible array.
+
+def get_digit_image(digit, small=False, color=False, white=False):
+    """Return the asset corresponding to the requested timer `digit`, as it
+    would appear in-game.
+
+    Parameters
+    ----------
+    digit : int
+        The digit (from 0 to 9) whose texture is to be returned.
+
+    small
+        Whether to return the texture scaled to the "small" size used for
+        centiseconds. (The default is the "large" size used for minutes and
+        seconds.)
+
+    color
+        Whether to return the texture in color. (The default is black and
+        white.)
+
+    white
+        Whether to return the texture on a white background. (The default is
+        black.)
+
+
+    Returns
+    -------
+    img : ndarray
+        The image of the appropriate digit as an OpenCV-compatible array.
     """
-    if n not in range(10):
+
+    if digit not in range(10):
         return None
 
     if white:
-        path = resource_string("times", "{0}_time_white.png".format(n))
+        path = resource_string("times", "{0}_time_white.png".format(digit))
     else:
-        path = resource_string("times", "{0}_time_small.png".format(n))
+        path = resource_string("times", "{0}_time_black.png".format(digit))
 
     nparr = np.frombuffer(path, np.uint8)
     img = cv2.imdecode(nparr, 1)
 
     if not color:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # The assets as used in-game are very slightly wider than the textures.
+    img = cv2.resize(img, (0, 0), fx=39/37, fy=1)
+
+    if small:
+        img = cv2.resize(img, (0, 0), fx=21/24, fy=21/24)
+    else:
+        img = cv2.resize(img, (0, 0), fx=26/24, fy=26/24)
 
     return img
 
@@ -82,88 +146,76 @@ def timer_values(start_time=480):
 class MeleeFrameSync(StreamParser):
     """
     """
-    def __init__(self, filename):
-        super().__init__(filename)
-
     @staticmethod
-    def get_frame_time(frame):
+    def match_digit(scene, small=False):
+        """Given a masked frame, estimate the uncovered digit.
+        """
+        matcher = TemplateMatcher(worst_match=0.45)
+
+        conf_array = [0 for n in range(10)]
+
+        for n in range(10):
+            digit = get_digit_image(n, small=small)
+            _, candidates = matcher.match(digit, scene, scale=1)
+            if candidates:
+                _, conf_black = max(candidates, key=lambda k: k[1])
+            else:
+                conf_black = 0
+
+            digit = get_digit_image(n, white=True, small=small)
+            _, candidates = matcher.match(digit, scene, scale=1)
+            if candidates:
+                _, conf_white = max(candidates, key=lambda k: k[1])
+            else:
+                conf_white = 0
+
+            conf_array[n] = max(conf_black, conf_white)
+
+        return max(range(10), key=lambda n: conf_array[n])
+
+    def get_frame_time(self, frame):
         """Read the timestamp of this frame.
         """
-        matcher = TemplateMatcher(debug=False, worst_match=0.45, criterion=cv2.TM_CCORR_NORMED)
-
         thresh_min = 200
         _, frame = cv2.threshold(frame, thresh_min, 255, cv2.THRESH_TOZERO)
 
-        digits = [None] * 6
-        boxes = [Rect(78, 515, 41, 42),
-                 Rect(78, 558, 41, 42),
-                 Rect(78, 621, 41, 42),
-                 Rect(78, 663, 41, 42),
-                 Rect(89, 718, 29, 28),
-                 Rect(89, 749, 29, 31)
+        boxes = [Rect(58, 245, 28, 26),
+                 Rect(58, 273, 28, 26),
+                 Rect(58, 315, 28, 26),
+                 Rect(58, 343, 28, 26),
+                 Rect(64, 377, 23, 21),
+                 Rect(64, 398, 23, 21)
                  ]
+        digits = [None for box in boxes]
+
         small = [False, False, False, False, True, True]
 
         for idx, _ in enumerate(digits):
-            mask = boxes[idx].to_mask(720, 1280, color=False)
+            mask = boxes[idx].to_mask(528, 643, color=False)
 
-            conf_array = [0 for _ in range(10)]
-            for n in range(10):
-                digit = get_digit_image(n, color=False)
-                digit = cv2.resize(digit, (0, 0), fx=39/37, fy=1)
-                # _, digit = cv2.threshold(digit, thresh_min, 255, cv2.THRESH_TOZERO)
-
-                if small[idx]:
-                    scale = 27/24
-                else:
-                    scale = 37/24
-                _, candidates = matcher.match(digit, frame * mask, scale=scale)
-
-                if candidates:
-                    _, best_conf = max(candidates, key=lambda k: k[1])
-
-                    conf_black = best_conf
-
-                digit = get_digit_image(n, color=False, white=True)
-                digit = cv2.resize(digit, (0, 0), fx=39/37, fy=1)
-                # _, digit = cv2.threshold(digit, thresh_min, 255, cv2.THRESH_TOZERO)
-
-                if small[idx]:
-                    scale = 27/24
-                else:
-                    scale = 37/24
-                _, candidates = matcher.match(digit, frame * mask, scale=scale)
-
-                if candidates:
-                    _, best_conf = max(candidates, key=lambda k: k[1])
-
-                    conf_white = best_conf
-
-                conf_array[n] = max(conf_black, conf_white)
-
-            digits[idx] = max(range(10), key=lambda n: conf_array[n])
-            print(digits[idx], ("{:.3f}\t"*10).format(*conf_array))
+            digits[idx] = self.match_digit(frame * mask, small=small[idx])
 
         if None not in digits:
             return "".join(str(d) for d in digits)
 
-    def read_frames(self):
-        for _ in range(1000):
-            yield self.get_frame_time(self.get_frame(color=False))
+        return None
 
 
-def main():
-    mfs = MeleeFrameSync('samples/slippi_test.mp4')
+def __main__():
+    mfs = MeleeFrameSync('samples/zelda-ic-fd.avi')
     realtimes = timer_values()
     realtime = next(realtimes)
 
-    while realtime != '075761':
-        realtime = next(realtimes)
+    for _ in range(103):
+        frame = mfs.get_frame()
 
-    for time in mfs.read_frames():
-        print(realtime, time, distance(realtime, time), flush=True)
+    for _ in range(1000):
+        frame = mfs.get_frame()
+        time = mfs.get_frame_time(frame)
+        print(time, realtime, distance(time, realtime))
+
         realtime = next(realtimes)
 
 
 if __name__ == '__main__':
-    main()
+    __main__()
